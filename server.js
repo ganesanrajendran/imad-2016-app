608740960e4cd36e1521ed1a91264e5f221f28a4
ganesanrajendran <ganesan.rajendiran@gmail.com>
2016-09-20 15:27:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var artivleName= req.params.articleName;
+    var articleName= req.params.articleName;
  res.send(createTemplate(articles[articleName]));
 });
 
