399412f69061d3a3f579af835d123be50281d594
ganesanrajendran <ganesan.rajendiran@gmail.com>
2016-09-22 17:27:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = 'Hi hasura.. Text Changeed';
+
+
+var elementImg = document.getElementById("img1");
+elementImg.onClick = function(){
+    elementImg.style.marginLeft='100px';
+};
