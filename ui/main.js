2ae0016fe4b0ff6762b473cc35156c99f6a0ba3b
ganesanrajendran <ganesan.rajendiran@gmail.com>
2016-09-22 18:37:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,19 @@
 var button = doucment.getElementById("counter");
 button.onclick = function(){
+    var requet = new XMLHttpRequest();
+    //create request
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status ===200){
+                var counter= request.responseText;
+                var span = document.getElementById("count");
+                span.innerHtml = counter.toString();
+            }  
+        }
+        
+    };
     
-    
-    counter = counter+ 1;
-    var span = document.getElementById("count");
-    span.innerHtml = counter.toString();
-}
+   //make request
+   request.open('GET', "http://ganesanrajendran.imad.hasura-app.io/counter");
+   request.send(null);
