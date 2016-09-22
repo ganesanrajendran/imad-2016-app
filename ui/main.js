5aa75f751de283e03f0d2d5ab95699860c9c1a58
ganesanrajendran <ganesan.rajendiran@gmail.com>
2016-09-22 19:17:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,7 @@ button.onclick = function(){
 
 //Submit Name
 
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit=document.getElementById("submit_btn");
 submit.onclick = function(){
    var request = new XMLHttpRequest();
@@ -45,6 +44,8 @@ submit.onclick = function(){
     };
     
    //make request
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
    request.open('GET', "http://ganesanrajendran.imad.hasura-app.io/submit-name?name=" + name, true);
    request.send(null);
 };
